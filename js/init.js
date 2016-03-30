121a93835a97d3ba47468514b4a9e4b8a876bc53
jgjadaoag <jgj.adaoag@gmail.com>
2016-03-31 02:52:28 +0800

    Fixed Angular Problem

--- js/init.js
+++ js/init.js
@@ -1,3 +1,4 @@
+'use strict';
 (function($){
   $(function(){
 
@@ -5,4 +6,19 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
-$('.modal-trigger').leanModal();
+
+let app = angular.module("recipeDesu", ["firebase"]);
+
+app.controller("recipeController", ["$scope", "$firebaseArray", ($scope, $firebaseArray) => {
+	let ref = new Firebase("https://flickering-fire-7415.firebaseio.com/"); 
+
+	$scope.recipes = $firebaseArray(ref);
+
+	$scope.addRecipe = () => {
+		$scope.recipes.$add({
+			name: $scope.recipeName,
+			classification: $scope.recipeClassification,
+			text: $scope.recipeText
+		});
+	};
+}]);
--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let myFirebaseRef = new Firebase("https://flickering-fire-7415.firebaseio.com/");
 
+/*
 let recipeSection = document.querySelector("#recipe-list");
 let template = document.querySelector("#recipe-template");
 let name = template.content.querySelector("h1");
@@ -11,14 +12,13 @@ let text = template.content.querySelector("article>p");
 myFirebaseRef.on("child_added", (snapshot) => {
 	  addRecipe(snapshot.val());
 });
+*/
 
-/*
 myFirebaseRef.push({
   name: "Hello World!",
   classification: "French",
   text: "Deep fry cookies, Done"
 });
-*/
 
 function addRecipe(recipe) {
 	name.textContent = recipe.name;
@@ -29,4 +29,6 @@ function addRecipe(recipe) {
 
 	recipeSection.appendChild(clone);
 }
+
+$('.modal-trigger').leanModal();
 
